01048be1e7c906f1788fa7350ba7ca59c2d11d79
ArvinLi <2118827858@qq.com>
2024-07-16 10:58:44 +0800

    flink chapter05 learn

---
+++ src/main/scala/cn/lgy/chapter05/SourceBoundTest.scala
@@ -0,0 +1,14 @@
+package cn.lgy.chapter05
+
+import org.apache.flink.streaming.api.scala._
+
+/**
+ * @description:
+ * @author: ArvinLi
+ * */
+object SourceBoundTest {
+  def main(args: Array[String]): Unit = {
+    //1.创建执行环境
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+  }
+}
